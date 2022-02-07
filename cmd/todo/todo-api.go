d487a39a084127f03d907f6eb7e82fa84f85d153
Allan Nava <allannava95@gmail.com>
2022-02-07 17:28:37 +0100

    fix todo api

--- cmd/todo/todo-api.go
+++ cmd/todo/todo-api.go
@@ -64,4 +64,22 @@ func main() {
 	//swagger setup
 	f.Get("/swagger/*", swagger.HandlerDefault) // default
 	//
+	f.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+		URL:         "http://example.com/doc.json",
+		DeepLinking: false,
+		// Expand ("list") or Collapse ("none") tag groups by default
+		DocExpansion: "none",
+		// Prefill OAuth ClientId on Authorize popup
+		OAuth: &swagger.OAuthConfig{
+			AppName:  "OAuth Provider",
+			ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
+		},
+		// Ability to change OAuth2 redirect uri location
+		OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
+	}))
+
+	fmt.Println("\nTodo CUSTOMER API")
+	fmt.Printf("\nENV: %s", config.CONFIGURATION.AppEnv)
+	fmt.Printf("\nRUNNING MODE: %s", config.CONFIGURATION.RunningMode)
+	f.Listen("0.0.0.0:8080")
 }
